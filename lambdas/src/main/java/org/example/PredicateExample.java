a91910c0779958b311ce1097d13a697e1dd11655
JL-GALE <jorge.galem@gmail.com>
2024-12-11 12:55:49 -0500

    Aplicacion de Lambda Predicate

---
+++ lambdas/src/main/java/org/example/PredicateExample.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        /*
+        * Precicate
+        * Resive un valor y devulve un booleano (true o false)
+        * */
+
+        Predicate<String> predicate = (param) -> param.equalsIgnoreCase("Hola");
+        System.out.println((predicate.test("HOLo")) ? "Son iguales" : "No son Iguales");
+
+        Predicate<String> predicate2 = (param) -> {
+            System.out.println(param.length());
+            return param.length() < 10;
+        };
+        System.out.println((predicate2.test("0123456789")) ? "Tiene menos de 10 caracteres" : "Tiene mas de 10 caracteres");
+    }
+}

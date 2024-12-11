6a3926991b2fc57c333f00f67fac1ce785a758f8
JL-GALE <jorge.galem@gmail.com>
2024-12-11 11:46:56 -0500

    Aplicacion de la lambda Funcion

---
+++ lambdas/src/main/java/org/example/FunctionExample.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.function.Function;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        /*
+        * Funtion
+        * Resive un valor y retorna un resultado
+        * */
+        Function<Byte, String> function = (param) -> "Tu edad es: ".concat(String.valueOf(param));
+        System.out.println(function.apply((byte)15));
+
+        Function<Byte, String> function2 = (param) -> (param < 18) ? "Eres menor de edad" : "Ya estas viejo en tu casa ya lledes";
+        System.out.println(function2.apply((byte)18));
+
+
+
+
+
+
+    }
+}

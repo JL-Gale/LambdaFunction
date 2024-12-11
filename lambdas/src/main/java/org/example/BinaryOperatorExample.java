9796134fb59fcb646b8fa7ff170d2510965507f6
JL-GALE <jorge.galem@gmail.com>
2024-12-11 13:09:18 -0500

    Aplicacion de lambda binaryOperator

---
+++ lambdas/src/main/java/org/example/BinaryOperatorExample.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        /*
+         *Binaryoperator
+         *Recibe dos valores del mismo tipo y retorna un valor del mismo tipo
+         **/
+        BinaryOperator<String> binaryOperator = (param1, param2) -> {
+            return param1.concat(" "+param2);
+        };
+        System.out.println(binaryOperator.apply("Hola", "Como estas"));
+
+        BinaryOperator<Integer> binaryOperator2 = (a, b) -> {
+            return a * b;
+        };
+        System.out.println(binaryOperator2.apply(5, 5));
+    }
+}

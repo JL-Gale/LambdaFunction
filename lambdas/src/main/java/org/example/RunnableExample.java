462276eddc50353c4a54028c8c6034858d39fc44
JL-GALE <jorge.galem@gmail.com>
2024-12-12 14:49:33 -0500

    Aplicando la funcion lambda Runnable

---
+++ lambdas/src/main/java/org/example/RunnableExample.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        /*
+        * Runnable
+        * No recibe valores y no retorna nada, solo ejecuta una tarea
+        * */
+        Runnable runnable = () -> {
+            System.out.println("Ejecutando tarea........");
+        };
+        runnable.run();
+
+    }
+}

2bebfe3f57918f89f07da3fad18910507e9c9f18
JL-GALE <jorge.galem@gmail.com>
2024-12-12 14:59:18 -0500

    Aplicacion de la funcion lambda Callable

---
+++ lambdas/src/main/java/org/example/CallableExample.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.concurrent.Callable;
+
+public class CallableExample {
+    public static void main(String[] args) {
+        /*
+        * Callable
+        * No recibe valores, pero retorna un resultado y  puede lanzar una excepcion.
+        * */
+
+        Callable<String> callable = () -> {
+            return "Callable preba";
+        };
+
+        try {
+            callable.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
